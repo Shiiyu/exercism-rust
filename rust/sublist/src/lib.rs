94d903a16825706cea9d52b79415f1653ff171c1
Shiiyu <faithiikittii@gmail.com>
2022-04-27 19:51:15 -0400

    refactor: :recycle: refactor sublist

--- rust/sublist/src/lib.rs
+++ rust/sublist/src/lib.rs
@@ -1,45 +1,36 @@
 #[derive(Debug, PartialEq)]
 pub enum Comparison {
   Equal,
+  Unequal,
   Sublist,
-  Superlist,
-  Unequal
+  Superlist
 }
 
 pub fn sublist<T: PartialEq>(first_list: &[T], second_list: &[T]) -> Comparison {
-  if first_list.is_empty() && second_list.is_empty() {
-    return Comparison::Equal;
-  } else if first_list.is_empty() {
-    return Comparison::Sublist;
-  } else if second_list.is_empty() {
-    return Comparison::Superlist;
-  }
-
-  if first_list.len() < second_list.len() {
-    let iter = second_list.windows(first_list.len());
-
-    for window in iter {
-      if first_list == window {
-        return Comparison::Sublist;
+  match (first_list.len(), second_list.len()) {
+    (0, 0) => Comparison::Equal,
+    (0, _) => Comparison::Sublist,
+    (_, 0) => Comparison::Superlist,
+    (first, second) if first > second => {
+      if first_list.windows(second).any(|w| w == second_list) {
+        Comparison::Superlist
+      } else {
+        Comparison::Unequal
       }
-    }
-
-    return Comparison::Unequal;
-  } else if first_list.len() > second_list.len() {
-    let iter = first_list.windows(second_list.len());
-
-    for window in iter {
-      if second_list == window {
-        return Comparison::Superlist;
+    },
+    (first, second) if first < second => {
+      if second_list.windows(first).any(|w| w == first_list) {
+        Comparison::Sublist
+      } else {
+        Comparison::Unequal
       }
-    }
-
-    return Comparison::Unequal;
-  } else {
-    if first_list == second_list {
-      return Comparison::Equal;
-    }
-
-    return Comparison::Unequal;
+    },
+    (_, _) => {
+      if first_list == second_list {
+        Comparison::Equal
+      } else {
+        Comparison::Unequal
+      }
+    },
   }
 }
