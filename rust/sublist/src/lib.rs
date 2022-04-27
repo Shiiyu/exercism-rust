99c030a990a73f907225308dad130c2bb73adcf7
Shiiyu <faithiikittii@gmail.com>
2022-04-27 19:39:42 -0400

    feat: :sparkles: sublist

---
+++ rust/sublist/src/lib.rs
@@ -0,0 +1,45 @@
+#[derive(PartialEq)]
+pub enum Comparison {
+  Equal,
+  Sublist,
+  Superlist,
+  Unequal
+}
+
+pub fn sublist<T: PartialEq>(first_list: &[T], second_list: &[T]) -> Comparison {
+  if first_list.is_empty() && second_list.is_empty() {
+    return Comparison::Equal;
+  } else if first_list.is_empty() {
+    return Comparison::Sublist;
+  } else if second_list.is_empty() {
+    return Comparison::Superlist;
+  }
+
+  if first_list.len() < second_list.len() {
+    let iter = second_list.windows(first_list.len());
+
+    for window in iter {
+      if first_list == window {
+        return Comparison::Sublist;
+      }
+    }
+
+    return Comparison::Unequal;
+  } else if first_list.len() > second_list.len() {
+    let iter = first_list.windows(second_list.len());
+
+    for window in iter {
+      if second_list == window {
+        return Comparison::Superlist;
+      }
+    }
+
+    return Comparison::Unequal;
+  } else {
+    if first_list == second_list {
+      return Comparison::Equal;
+    }
+
+    return Comparison::Unequal;
+  }
+}
