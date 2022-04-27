96a2cde61346031f8fab01b4cdd942805fff4e73
Shiiyu <faithiikittii@gmail.com>
2022-04-26 23:27:45 -0400

    refactor: :recycle: refactor assembly line

--- rust/assembly-line/src/lib.rs
+++ rust/assembly-line/src/lib.rs
@@ -1,19 +1,18 @@
-// This stub file contains items that aren't used yet; feel free to remove this module attribute
-// to enable stricter warnings.
+// This stub file contains items that aren't used yet; feel free to remove this
+// module attribute to enable stricter warnings.
 #![allow(unused)]
 
 pub fn production_rate_per_hour(speed: u8) -> f64 {
-    let speed_float = speed as f64;
-
-    if speed < 5 {
-        return speed_float * 221.0;
-    } else if speed < 9 {
-        return speed_float * 221.0 * 0.9;
+  speed as f64
+    * 221
+    * match speed {
+      0..=4 => 1.0,
+      5..=8 => 0.9,
+      9..=10 => 0.77,
+      _ => unreachable!()
     }
-
-    speed_float * 221.0 * 0.77
 }
 
 pub fn working_items_per_minute(speed: u8) -> u32 {
-    production_rate_per_hour(speed) as u32 / 60
+  production_rate_per_hour(speed) as u32 / 60
 }
