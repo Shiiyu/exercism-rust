3ce86e51b1ab005ba2af2f2c70248010271dbbfa
Shiiyu <faithiikittii@gmail.com>
2022-04-26 23:28:01 -0400

    style: :art: add formatting

--- rust/short-fibonacci/src/lib.rs
+++ rust/short-fibonacci/src/lib.rs
@@ -1,28 +1,30 @@
 /// Create an empty vector
 pub fn create_empty() -> Vec<u8> {
-    Vec::new()
+  Vec::new()
 }
 
 /// Create a buffer of `count` zeroes.
 ///
-/// Applications often use buffers when serializing data to send over the network.
+/// Applications often use buffers when serializing data to send over the
+/// network.
 pub fn create_buffer(count: usize) -> Vec<u8> {
-    vec![0; count]
+  vec![0; count]
 }
 
-/// Create a vector containing the first five elements of the Fibonacci sequence.
+/// Create a vector containing the first five elements of the Fibonacci
+/// sequence.
 ///
-/// Fibonacci's sequence is the list of numbers where the next number is a sum of the previous two.
-/// Its first five elements are `1, 1, 2, 3, 5`.
+/// Fibonacci's sequence is the list of numbers where the next number is a sum
+/// of the previous two. Its first five elements are `1, 1, 2, 3, 5`.
 pub fn fibonacci() -> Vec<u8> {
-    let mut fibb = create_buffer(5);
+  let mut fibb = create_buffer(5);
 
-    for i in 0..5 {
-        match i {
-            0..=1 => fibb[i] = 1,
-            _ => fibb[i] = fibb[i - 2] + fibb[i - 1],
-        }
+  for i in 0..5 {
+    match i {
+      0..=1 => fibb[i] = 1,
+      _ => fibb[i] = fibb[i - 2] + fibb[i - 1]
     }
+  }
 
-    fibb
+  fibb
 }
