bd045d92c80d4b2115e7d0db981c1187f04dea0d
Shiiyu <faithiikittii@gmail.com>
2022-04-27 22:48:10 -0400

    feat: :sparkles: allergies

---
+++ rust/allergies/src/lib.rs
@@ -0,0 +1,33 @@
+use crate::Allergen::*;
+
+pub struct Allergies(Vec<Allergen>);
+
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub enum Allergen {
+  Eggs = 1,
+  Peanuts = 2,
+  Shellfish = 4,
+  Strawberries = 8,
+  Tomatoes = 16,
+  Chocolate = 32,
+  Pollen = 64,
+  Cats = 128
+}
+
+impl Allergen {
+  const ALLERGENS: [Allergen; 8] = [Cats, Pollen, Chocolate, Tomatoes, Strawberries, Shellfish, Peanuts, Eggs];
+}
+
+impl Allergies {
+  pub fn new(score: u32) -> Self {
+    Allergies(Allergen::ALLERGENS.iter().copied().filter(|allergen| (score & *allergen as u32) != 0).collect())
+  }
+
+  pub fn is_allergic_to(&self, allergen: &Allergen) -> bool {
+    self.0.contains(allergen)
+  }
+
+  pub fn allergies(&self) -> Vec<Allergen> {
+    self.0.clone()
+  }
+}
