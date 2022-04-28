7001e6e594e208261258e3f610f15d82ceb77a74
Shiiyu <faithiikittii@gmail.com>
2022-04-28 00:20:20 -0400

    refactor: :recycle: refactor allergies

--- rust/allergies/src/lib.rs
+++ rust/allergies/src/lib.rs
@@ -1,6 +1,4 @@
-use crate::Allergen::*;
-
-pub struct Allergies(Vec<Allergen>);
+pub struct Allergies(u8);
 
 #[derive(Copy, Clone, Debug, PartialEq)]
 pub enum Allergen {
@@ -15,19 +13,39 @@ pub enum Allergen {
 }
 
 impl Allergen {
-  const ALLERGENS: [Allergen; 8] = [Eggs, Peanuts, Shellfish, Strawberries, Tomatoes, Chocolate, Pollen, Cats];
+  fn from(val: u8) -> Self {
+    match val {
+      1 => Allergen::Eggs,
+      2 => Allergen::Peanuts,
+      4 => Allergen::Shellfish,
+      8 => Allergen::Strawberries,
+      16 => Allergen::Tomatoes,
+      32 => Allergen::Chocolate,
+      64 => Allergen::Pollen,
+      128 => Allergen::Cats,
+      _ => panic!("Invalid Allergen")
+    }
+  }
 }
 
 impl Allergies {
   pub fn new(score: u32) -> Self {
-    Self(Allergen::ALLERGENS.iter().copied().filter(|allergen| (score & *allergen as u32) != 0).collect())
+    Self(score as u8)
   }
 
   pub fn is_allergic_to(&self, allergen: &Allergen) -> bool {
-    self.0.contains(allergen)
+    self.0 & *allergen as u8 == *allergen as u8
   }
 
   pub fn allergies(&self) -> Vec<Allergen> {
-    self.0.clone()
+    let mut allergies = Vec::new();
+
+    (0..8).into_iter().for_each(|i| {
+      if self.0 & (1 << i) > 0 {
+        allergies.push(Allergen::from(1 << i))
+      }
+    });
+
+    allergies
   }
 }
