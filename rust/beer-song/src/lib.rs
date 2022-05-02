bed1aacde55ed18bc1508242d6365392c5bc7e0b
Shiiyu <faithiikittii@gmail.com>
2022-05-02 17:42:17 -0400

    feat: :sparkles: beer song

---
+++ rust/beer-song/src/lib.rs
@@ -0,0 +1,26 @@
+const VERSES: [&'static str; 3] = [
+  "No more bottles of beer on the wall, no more bottles of beer.\n\
+    Go to the store and buy some more, 99 bottles of beer on the wall.\n",
+  "1 bottle of beer on the wall, 1 bottle of beer.\n\
+    Take it down and pass it around, no more bottles of beer on the wall.\n",
+  "2 bottles of beer on the wall, 2 bottles of beer.\n\
+    Take one down and pass it around, 1 bottle of beer on the wall.\n"
+];
+
+pub fn verse(n: u16) -> String {
+  match n {
+    0 => VERSES[0].to_string(),
+    1 => VERSES[1].to_string(),
+    2 => VERSES[2].to_string(),
+    _ => format!(
+      "{0} bottles of beer on the wall, {0} bottles of beer.\n\
+      Take one down and pass it around, {1} bottles of beer on the wall.\n",
+      n,
+      n - 1
+    )
+  }
+}
+
+pub fn sing(start: u16, end: u16) -> String {
+  (end..=start).rev().map(|v| verse(v)).collect::<Vec<String>>().join("\n")
+}
