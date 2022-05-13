8b5506de6696fdad86cebc765be94fff749c5658
Shiiyu <faithiikittii@gmail.com>
2022-05-12 23:27:56 -0400

    feat: :sparkles: matching brackets

---
+++ rust/matching-brackets/src/lib.rs
@@ -0,0 +1,32 @@
+fn equals(a: char, b: char) -> bool {
+  match a {
+    '(' => b == ')',
+    '[' => b == ']',
+    '{' => b == '}',
+    _ => unreachable!()
+  }
+}
+
+pub fn brackets_are_balanced(string: &str) -> bool {
+  let mut openings = Vec::new();
+
+  for bracket in string.chars() {
+    if matches!(bracket, '(' | '[' | '{') {
+      openings.push(bracket);
+    } else if !openings.is_empty() && equals(openings[openings.len() - 1], bracket) {
+      openings.pop();
+    } else if matches!(bracket, ')' | ']' | '}') {
+      return false;
+    } else if !matches!(bracket, '(' | '[' | '{') {
+      continue;
+    } else {
+      return false;
+    }
+  }
+
+  if !openings.is_empty() {
+    return false;
+  }
+
+  true
+}
